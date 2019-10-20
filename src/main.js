eb5d674605e84b41ee37ee3c494fb7b3facf3673
long.le1 <long.le1@mservice.com.vn>
2019-10-20 09:46:11 +0700

    Create project. Fixed render twice on home page. Test bootstrap.

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Home from './components/Home'
+import BootstrapVue from 'bootstrap-vue'
+import router from './router/router.ts'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+// Entry point is main.js which is a gateway
+
+
+Vue.use(BootstrapVue)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+  // template: '<App/>',
+  // components: { App }
+})

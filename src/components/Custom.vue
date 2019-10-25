706a5787df84ec1ade1f9cc3a3160dd62c889537
long.le1 <long.le1@mservice.com.vn>
2019-10-26 02:32:10 +0700

    Test hhtp requests.

--- src/components/Custom.vue
+++ src/components/Custom.vue
@@ -2,11 +2,22 @@
   <div id="custom">
     <p> Custom</p>
     <b-button variant="success">Button</b-button>
+    {{info}}
   </div>
 </template>
 
 <script>
 export default {
+  name: 'custom',
+  data(){
+    return{
+      info: null
+    }
+  },
+  mounted(){
+    axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(response=>this.info = response)
+  }
 }
 </script>
 <style>
